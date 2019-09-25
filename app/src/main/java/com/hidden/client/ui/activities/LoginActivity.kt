e5a3d3b6299903c26f3a065ea23cd3bb4f6bf46e
rango527 <web.mobile497373@gmail.com>
2019-09-26 01:51:31 +0900

    add login activity

--- app/src/main/java/com/hidden/client/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/hidden/client/ui/activities/LoginActivity.kt
@@ -1,13 +1,35 @@
 package com.hidden.client.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import com.hidden.client.R
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Get the widgets
+        val textForgotPassword = findViewById<TextView>(R.id.text_forgot_password)
+
+        // Set a click listener
+        textForgotPassword.setOnClickListener(this);
+    }
+
+    override fun onClick(v: View?) {
+
+        val id = v!!.id
+
+        when(id){
+            R.id.text_forgot_password->{
+                val intent = Intent(applicationContext, ForgetPasswordActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
