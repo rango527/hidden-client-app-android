bfb92e3a5a35d8515b0efc8e8d8402f0106f1fc1
rango527 <web.mobile497373@gmail.com>
2019-09-26 19:02:05 +0900

    fotgot password activity

--- app/src/main/java/com/hidden/client/ui/activities/ForgetPasswordActivity.kt
+++ app/src/main/java/com/hidden/client/ui/activities/ForgetPasswordActivity.kt
@@ -1,13 +1,33 @@
 package com.hidden.client.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import com.hidden.client.R
 
-class ForgetPasswordActivity : AppCompatActivity() {
-
+class ForgetPasswordActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forget_password)
+
+        // Get the widgets
+        val button_backToLogin = findViewById<ImageButton>(R.id.button_backToLogin)
+
+        // Set a click listener
+        button_backToLogin.setOnClickListener(this);
+    }
+
+    override fun onClick(v: View?) {
+        val id = v!!.id
+
+        when(id){
+            R.id.button_backToLogin->{
+                val intent = Intent(applicationContext, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
