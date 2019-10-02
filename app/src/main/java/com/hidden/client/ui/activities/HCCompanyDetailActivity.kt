f6faaecf3337c4a2b538d259c88b0456ba8b5fd4
rango527 <web.mobile497373@gmail.com>
2019-10-03 00:42:42 +0900

    company view activity

---
+++ app/src/main/java/com/hidden/client/ui/activities/HCCompanyDetailActivity.kt
@@ -0,0 +1,18 @@
+package com.hidden.client.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.hidden.client.R
+
+class HCCompanyDetailActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_company_detail)
+    }
+
+    override fun onClick(v: View?) {
+
+    }
+}
--- app/src/main/java/com/hidden/client/ui/activities/HcYourJobDetailActivity.kt
+++ app/src/main/java/com/hidden/client/ui/activities/HcYourJobDetailActivity.kt
@@ -1,18 +1,38 @@
 package com.hidden.client.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import com.hidden.client.R
 
 class HcYourJobDetailActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var btnBackToYourJob: ImageButton
+    private lateinit var layoutShowCompanyDetail: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_your_job_detail)
+
+        btnBackToYourJob = findViewById(R.id.button_back_to_your_job)
+        btnBackToYourJob.setOnClickListener(this)
+
+        layoutShowCompanyDetail = findViewById(R.id.layout_show_company_detail)
+        layoutShowCompanyDetail.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-
+        when (v!!.id) {
+            R.id.button_back_to_your_job -> {
+                finish()
+            }
+            R.id.layout_show_company_detail -> {
+                val intent = Intent(applicationContext, HCCompanyDetailActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
