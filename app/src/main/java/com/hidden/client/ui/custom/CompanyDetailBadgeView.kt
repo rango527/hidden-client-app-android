40d029ba76888a05deb9de7f13a2cc39e6f93e8e
rango527 <web.mobile497373@gmail.com>
2019-10-03 12:23:27 +0900

    add CompanyDetailBadgeView

---
+++ app/src/main/java/com/hidden/client/ui/custom/CompanyDetailBadgeView.kt
@@ -0,0 +1,42 @@
+package com.hidden.client.ui.custom
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.util.AttributeSet
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.hidden.client.R
+
+class CompanyDetailBadgeView (context: Context, attrs: AttributeSet): LinearLayout(context, attrs) {
+
+    init {
+        inflate(context, R.layout.company_detail_badge_view, this)
+
+        var layoutBackground: LinearLayout = findViewById(R.id.layout_background)
+        val textTitle: TextView = findViewById(R.id.text_title)
+        val imgIcon: ImageView = findViewById(R.id.img_icon)
+
+        val attributes = context.obtainStyledAttributes(attrs, R.styleable.CompanyDetailBadge)
+
+        textTitle.text = attributes.getString(R.styleable.CompanyDetailBadge_badge_title)
+
+        var iconDrawable: Drawable? = attributes.getDrawable(R.styleable.CompanyDetailBadge_badge_icon)
+        if (iconDrawable != null) {
+            imgIcon.setImageDrawable(iconDrawable)
+        }
+
+        var backgroundDrawable: Drawable? = attributes.getDrawable(R.styleable.CompanyDetailBadge_badge_color)
+        if (backgroundDrawable != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                layoutBackground.background = backgroundDrawable
+            } else {
+                @Suppress("DEPRECATION")
+                layoutBackground.setBackgroundDrawable(backgroundDrawable)
+            }
+        }
+
+        attributes.recycle()
+    }
+}
