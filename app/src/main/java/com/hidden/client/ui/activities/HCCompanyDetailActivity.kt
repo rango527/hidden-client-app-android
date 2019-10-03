177f87e1ddd3227e7241079ec706dc3621520912
rango527 <web.mobile497373@gmail.com>
2019-10-03 17:33:15 +0900

    fix CompanyDetailView activity (add close button)

--- app/src/main/java/com/hidden/client/ui/activities/HCCompanyDetailActivity.kt
+++ app/src/main/java/com/hidden/client/ui/activities/HCCompanyDetailActivity.kt
@@ -3,16 +3,26 @@ package com.hidden.client.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import com.hidden.client.R
 
 class HCCompanyDetailActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var imgClose:ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_company_detail)
+
+        imgClose = findViewById(R.id.img_close)
+        imgClose.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-
+        when(v!!.id) {
+            R.id.img_close -> {
+                finish()
+            }
+        }
     }
 }
