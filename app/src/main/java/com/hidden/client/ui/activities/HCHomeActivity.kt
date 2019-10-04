3f8898098ec299b16d344f48d4d745de85638690
rango527 <web.mobile497373@gmail.com>
2019-10-04 19:23:54 +0900

    fix navbar

--- app/src/main/java/com/hidden/client/ui/activities/HCHomeActivity.kt
+++ app/src/main/java/com/hidden/client/ui/activities/HCHomeActivity.kt
@@ -1,8 +1,14 @@
 package com.hidden.client.ui.activities
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -12,8 +18,21 @@ import com.hidden.client.helpers.HCGlobal
 import com.hidden.client.ui.fragments.home.dashboard.HCDashboardFragment
 import com.hidden.client.ui.fragments.home.processes.HCProcessesFragment
 import com.hidden.client.ui.fragments.home.shortlists.HCShortlistsFragment
+import kotlinx.android.synthetic.main.list_row_circle_image.*
 
-class HCHomeActivity : AppCompatActivity() {
+class HCHomeActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var tabDashboard: LinearLayout
+    private lateinit var tabShortlists: LinearLayout
+    private lateinit var tabProcesses: LinearLayout
+
+    private lateinit var imgDashboard: ImageView
+    private lateinit var imgShortlists: ImageView
+    private lateinit var imgProcesses: ImageView
+
+    private lateinit var textDashboard: TextView
+    private lateinit var textShortlists: TextView
+    private lateinit var textProcesses: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,34 +40,76 @@ class HCHomeActivity : AppCompatActivity() {
 
         HCGlobal.getInstance(this).g_currentActivity = this
 
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        tabDashboard = findViewById(R.id.layout_tab_dashboard)
+        tabDashboard.setOnClickListener(this)
+
+        tabShortlists = findViewById(R.id.layout_tab_shortlists)
+        tabShortlists.setOnClickListener(this)
+
+        tabProcesses = findViewById(R.id.layout_tab_processes)
+        tabProcesses.setOnClickListener(this)
+
+        imgDashboard = findViewById(R.id.img_tab_dashboard)
+        imgShortlists = findViewById(R.id.img_tab_shortlists)
+        imgProcesses = findViewById(R.id.img_tab_processes)
+
+        textDashboard = findViewById(R.id.text_tab_dashboard)
+        textShortlists = findViewById(R.id.text_tab_shortlists)
+        textProcesses = findViewById(R.id.text_tab_processes)
 
         if (savedInstanceState == null) {
-            navView.selectedItemId = R.id.navigation_shortlists
-            supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, HCShortlistsFragment()).commit()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, HCShortlistsFragment()).commit()
+
+            imgShortlists.setImageResource(R.drawable.menu_shortlists_filled)
+            textShortlists.setTextColor(ContextCompat.getColor(this, R.color.colorBlack_1))
+        }
+
+
+    }
+
+    override fun onClick(v: View?) {
+
+        when (v!!.id) {
+            R.id.layout_tab_dashboard -> {
+
+                setTabButtonDefault()
+
+                imgDashboard.setImageResource(R.drawable.menu_dashboard)
+                textDashboard.setTextColor(ContextCompat.getColor(this, R.color.colorBlack_1))
+
+                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, HCDashboardFragment()).commit()
+            }
+            R.id.layout_tab_shortlists -> {
+
+                setTabButtonDefault()
+
+                imgShortlists.setImageResource(R.drawable.menu_shortlists_filled)
+                textShortlists.setTextColor(ContextCompat.getColor(this, R.color.colorBlack_1))
+
+                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, HCShortlistsFragment()).commit()
+            }
+            R.id.layout_tab_processes -> {
+
+                setTabButtonDefault()
+
+                imgProcesses.setImageResource(R.drawable.menu_processes_filled)
+                textProcesses.setTextColor(ContextCompat.getColor(this, R.color.colorBlack_1))
+
+                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, HCProcessesFragment()).commit()
+            }
         }
     }
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+    private fun setTabButtonDefault() {
 
-        when (menuItem.itemId) {
-            R.id.navigation_dashboard -> {
-                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, HCDashboardFragment()).commit()
+        imgDashboard.setImageResource(R.drawable.menu_dashboard_empty)
+        textDashboard.setTextColor(ContextCompat.getColor(this, R.color.colorGray_3))
 
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_shortlists -> {
-                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, HCShortlistsFragment()).commit()
+        imgShortlists.setImageResource(R.drawable.menu_shortlists_empty)
+        textShortlists.setTextColor(ContextCompat.getColor(this, R.color.colorGray_3))
 
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_processes -> {
-                supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, HCProcessesFragment()).commit()
-
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
+        imgProcesses.setImageResource(R.drawable.menu_processes_empty)
+        textProcesses.setTextColor(ContextCompat.getColor(this, R.color.colorGray_3))
     }
 }
--- app/src/main/java/com/hidden/client/ui/activities/HCProcessActivity.kt
+++ app/src/main/java/com/hidden/client/ui/activities/HCProcessActivity.kt
@@ -2,12 +2,94 @@ package com.hidden.client.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.hidden.client.R
+import com.hidden.client.ui.fragments.home.shortlists.HCShortlistsFragment
+import com.hidden.client.ui.fragments.process.HCMessageFragment
+import com.hidden.client.ui.fragments.process.HCProcessFragment
+import kotlinx.android.synthetic.main.activity_process.*
+import kotlinx.android.synthetic.main.activity_splash.*
 
-class HCProcessActivity : AppCompatActivity() {
+class HCProcessActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var textBtnProcess: TextView
+    private lateinit var textBtnMessage: TextView
+
+    private lateinit var imgPhoto: ImageView
+    private lateinit var textName: TextView
+    private lateinit var textFor: TextView
+
+    private lateinit var fragmentProcess: FrameLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_process)
+
+        fragmentProcess = findViewById(R.id.fragment_process)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_process, HCProcessFragment()).commit()
+        }
+
+        // Init View
+        imgPhoto = findViewById(R.id.img_photo)
+        textName = findViewById(R.id.text_name)
+        textFor = findViewById(R.id.text_for)
+
+        textBtnProcess = findViewById(R.id.text_process)
+        textBtnProcess.setOnClickListener(this)
+
+        textBtnMessage = findViewById(R.id.text_message)
+        textBtnMessage.setOnClickListener(this)
+
     }
+
+    override fun onClick(v: View?) {
+        when(v!!.id) {
+            R.id.text_process -> {
+                textBtnProcess.setBackgroundResource(R.drawable.panel_top_rounded_border_small)
+                textBtnProcess.setTextColor(ContextCompat.getColor(this, R.color.colorBlack_2))
+
+                textBtnMessage.setBackgroundResource(android.R.color.transparent)
+                textBtnMessage.setTextColor(ContextCompat.getColor(this, R.color.colorWhite_1))
+
+                textName.visibility = View.VISIBLE
+                textFor.visibility = View.VISIBLE
+                imgPhoto.layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
+
+                val param = imgPhoto.layoutParams as LinearLayout.LayoutParams
+                param.topMargin = resources.getDimension(R.dimen.margin_default).toInt()
+                imgPhoto.layoutParams = param
+
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_process, HCProcessFragment()).commit()
+            }
+
+            R.id.text_message -> {
+                textBtnProcess.setBackgroundResource(android.R.color.transparent)
+                textBtnProcess.setTextColor(ContextCompat.getColor(this, R.color.colorWhite_1))
+
+                textBtnMessage.setBackgroundResource(R.drawable.panel_top_rounded_border_small)
+                textBtnMessage.setTextColor(ContextCompat.getColor(this, R.color.colorBlack_2))
+
+                textName.visibility = View.GONE
+                textFor.visibility = View.GONE
+
+                var density: Float = applicationContext.resources.displayMetrics.density
+                imgPhoto.layoutParams.height = Math.round(40 * density)
+
+                val param = imgPhoto.layoutParams as LinearLayout.LayoutParams
+                param.topMargin = resources.getDimension(R.dimen.list_row_margin_default).toInt()
+                imgPhoto.layoutParams = param
+
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_process, HCMessageFragment()).commit()
+            }
+        }
+    }
+
 }
