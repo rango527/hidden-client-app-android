991a1ed589cbd002769323c8b35a2ed83c6077fb
rango527 <web.mobile497373@gmail.com>
2019-10-16 10:11:52 +0900

    implement terms service api

--- app/src/main/java/com/hidden/client/ui/activities/settings/HCTermsOfServiceActivity.kt
+++ app/src/main/java/com/hidden/client/ui/activities/settings/HCTermsOfServiceActivity.kt
@@ -1,14 +1,54 @@
 package com.hidden.client.ui.activities.settings
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Html
+import android.widget.TextView
+import android.widget.Toast
 import com.hidden.client.R
+import com.hidden.client.apis.RetrofitClient
+import com.hidden.client.datamodels.HCConsentResponse
+import com.hidden.client.enums.Consent
+import com.hidden.client.enums.UserType
 import com.hidden.client.ui.HCBaseActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HCTermsOfServiceActivity : HCBaseActivity() {
+
+    private lateinit var txtTerms: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_terms_of_service)
+
+        txtTerms = findViewById(R.id.text_value)
+
+        initCloseButton()
+
+        RetrofitClient.instance.getConsent(UserType.client.toString(), Consent.terms.toString())
+            .enqueue(object: Callback<HCConsentResponse> {
+                override fun onFailure(call: Call<HCConsentResponse>, t: Throwable) {
+                    Toast.makeText(applicationContext, "Failed...", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onResponse(
+                    call: Call<HCConsentResponse>,
+                    response: Response<HCConsentResponse>
+                ) {
+
+                    if (response.isSuccessful) {
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                            txtTerms.setText(Html.fromHtml(response.body()!!.content, Html.FROM_HTML_MODE_COMPACT))
+                        } else {
+                            txtTerms.setText(Html.fromHtml(response.body()!!.content))
+                        }
+                    } else {
+                        Toast.makeText(applicationContext, "Error", Toast.LENGTH_LONG).show()
+                    }
+                }
+            })
     }
 }
