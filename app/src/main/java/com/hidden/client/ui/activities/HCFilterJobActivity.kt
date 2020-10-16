44b682eef064896ec3318a71db8a7e691c821430
rango527 <web.mobile497373@gmail.com>
2020-10-16 10:54:08 +0800

    message attach file, take photo and video

--- app/src/main/java/com/hidden/client/ui/activities/HCFilterJobActivity.kt
+++ app/src/main/java/com/hidden/client/ui/activities/HCFilterJobActivity.kt
@@ -2,12 +2,17 @@ package com.hidden.client.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.hidden.client.R
 
-class HCFilterJobActivity : AppCompatActivity() {
+class HCFilterJobActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filter_job)
+    }
+
+    override fun onClick(v: View?) {
+
     }
 }
--- app/src/main/java/com/hidden/client/ui/fragments/process/HCMessageFragment.kt
+++ app/src/main/java/com/hidden/client/ui/fragments/process/HCMessageFragment.kt
@@ -20,7 +20,10 @@ import com.hidden.client.helpers.HCDialog
 import com.hidden.client.helpers.HCGlobal
 import com.hidden.client.ui.activities.ConversationFileAttachActivity
 import com.hidden.client.ui.activities.ConversationTakePhotoActivity
+import com.hidden.client.ui.activities.ConversationTakePhototestActivity
 import com.hidden.client.ui.activities.HCProcessFilterActivity
+import com.hidden.client.ui.dialogs.BottomAddInterviewerToInterviewDialog
+import com.hidden.client.ui.dialogs.BottomAddMediaPickerDialog
 import com.hidden.client.ui.viewmodels.injection.ViewModelFactory
 import com.hidden.client.ui.viewmodels.main.MessageListVM
 import com.kaopiz.kprogresshud.KProgressHUD
@@ -38,20 +41,9 @@ class HCMessageFragment(
     private lateinit var attachFileBtn: ImageView
     private lateinit var takePhotoBtn: ImageView
 
-    private lateinit var filePathTxt: TextView
-    private lateinit var filePath: Uri
-
-    //
-    private var mediaPath: String? = null
-    private var postPath: String? = null
     private lateinit var imageView: ImageView
 
     private val CAPTURE_REQUEST_CODE = 42
-    private var REQUEST_PICK_PHOTO: Int = 0
-
-    private val TAKE_PHOTO_REQUEST = 101
-    private var mCurrentPhotoPath: String = ""
-//    private lateinit var swipeContainer: SwipeRefreshLayout
 
     private lateinit var progressDlg: KProgressHUD
 
@@ -85,9 +77,6 @@ class HCMessageFragment(
 
         binding.recyclerviewProcesses.layoutManager = LinearLayoutManager(context!!)
 
-        layoutBtnFilterSearch = view.findViewById(R.id.layout_filter_search)
-        layoutBtnFilterSearch.setOnClickListener(this)
-
         messageSendBtn = view.findViewById(R.id.message_send_button)
         messageSendBtn.setOnClickListener(this)
 
@@ -103,15 +92,10 @@ class HCMessageFragment(
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.layout_filter_search -> {
-                val intent = Intent(context, HCProcessFilterActivity::class.java)
-            }
-
             R.id.message_send_button -> {
                 val message = edit_text_message.text.toString()
                 if (message == "") {
 //                    Toast.makeText(this@HCMessageFragment, "Please write a message, first...", Toast.LENGTH_LONG).show()
-
                 } else {
                     viewModel.sendMessage(conversationId, message)
                     edit_text_message.setText("")
@@ -119,47 +103,16 @@ class HCMessageFragment(
             }
 
             R.id.file_attachment -> {
-//                val intent = Intent()
-//                intent.setType("image/*")
-//                intent.setAction(Intent.ACTION_GET_CONTENT)
-//                startActivityForResult(Intent.createChooser(intent, "Choose Picture"), 111)
-
-//                val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-//                startActivityForResult(galleryIntent, REQUEST_PICK_PHOTO)
                 val intent = Intent(HCGlobal.getInstance().currentActivity, ConversationFileAttachActivity::class.java)
                 intent.putExtra("conversationId", conversationId)
                 HCGlobal.getInstance().currentActivity.startActivity(intent)
             }
 
             R.id.take_photo -> {
-//                captureImage()
-//                val capture = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//                startActivityForResult(capture, CAPTURE_REQUEST_CODE)
-
-                val intent = Intent(HCGlobal.getInstance().currentActivity, ConversationTakePhotoActivity::class.java)
-                intent.putExtra("conversationId", conversationId)
-                HCGlobal.getInstance().currentActivity.startActivity(intent)
+                BottomAddMediaPickerDialog.display(activity!!.supportFragmentManager, conversationId);
             }
         }
     }
-//
-//    private fun captureImage() {
-//        if (Build.VERSION.SDK_INT > 21) {
-//            val callCameraApplicationIntent = Intent()
-//            callCameraApplicationIntent.action = MediaStore.ACTION_IMAGE_CAPTURE
-//
-//            var photoFile: File? = null
-//            try {
-//                photoFile = createImageFile()
-//            } catch (e: IOException) {
-//                Logger.getAnonymousLogger().info("Exception error")
-//                e.printStackTrace()
-//            }
-//
-//            val outputUri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", photoFile)
-//
-//        }
-//    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (requestCode == CAPTURE_REQUEST_CODE) {
@@ -171,36 +124,4 @@ class HCMessageFragment(
             super.onActivityResult(requestCode, resultCode, data)
         }
     }
-//
-//    protected fun initDialog() {
-//        pDialog = ProgressDialog(this)
-//        pDialog.setMessage("Loading...")
-//        pDialog.setCancelable(true)
-//    }
-//
-//    protected fun showpDialog() {
-//        if (!pDialog.isShowing)
-//            pDialog.show()
-//    }
-//
-//    protected fun hidepDialog() {
-//        if (pDialog.isShowing) pDialog.dismiss()
-//    }
-//
-//    private fun upload() {
-//
-//    }
-//
-//    companion object {
-//        private val REQUEST_TAKE_PHOTO = 0
-//        private val REQUEST_PICK_PHOTO = 2
-//        private val CAMERA_PIC_REQUEST = 1111
-//
-//        private val TAG = HCMessageFragment::class.java.getSimpleName()
-//
-//        private val CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100
-//
-//
-//    }
-
 }
